987bbc53baa40fb31510f36b9bb5ecd501358053
Daniyar-qrbn <dnyr_qurban@outlook.com>
2026-03-24 01:32:12 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <Person />
+</template>
+
+<script>
+    import Person from './components/Person.vue'
+    export default {
+        name: 'App', //组建名
+        components: { Person }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Person.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="person">
+        <h2>姓名:{{name}}</h2>
+        <h2>年龄:{{age}}</h2>
+        <h2>电话:{{tel}}</h2>
+        <button @click="changeName">修改名字</button>
+        <button @click="changeAge">修改年龄</button>
+        <button @click="show">查看</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Person',
+        setup() {
+            // 数据
+            let name = '达尼亚尔' //此时的name不是响应式的
+            let age = 23 //此时的age不是响应式的
+            let tel = '176'
+            // 方法
+            function changeName() {
+                name = '达尼亚尔·库尔班江'
+            }
+            function changeAge() {
+                age++
+            }
+            function show() {
+                console.log("as")
+                alert(tel)
+            }
+            return { name, age, tel, changeAge, changeName, show }
+        }
+    }
+</script>
+
+<style>
+    .person {
+        background-color: #39b3e7;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+
+    button {
+        margin: 0 5px;
+    }
+</style>
